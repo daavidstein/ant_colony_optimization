dccff58f6eccbf96016b3de18422158ea829e3a3
Aaron Niskin <amniskin@gmail.com>
2025-03-02 15:42:25 -0800

    wip

--- src/ant_colony_optimization/core.py
+++ src/ant_colony_optimization/core.py
@@ -2,53 +2,61 @@ import mesa
 from mesa.space import MultiGrid, PropertyLayer
 
 
-
 class Model(mesa.Model):
-    def __init__(self, size: int, n_ants: int, ant_vision = 1, n_foodpile = 1):
+    def __init__(self, size: int, n_ants: int, ant_vision=1, n_foodpile=1):
         super().__init__()
         self.n_ants = n_ants
 
         food_layer = self.init_food(size, n_ants)
-        pheromone_layer = PropertyLayer(name="pheromone", width = size, height= size, default_value=0, dtype=int)
-        self.grid = MultiGrid(width=size, height=size, torus=True, 
-                              property_layers=[food_layer, pheromone_layer])
-        self.anthill_pos = (size//2, size//2)
+        pheromone_layer = PropertyLayer(
+            name="pheromone", width=size, height=size, default_value=0, dtype=int
+        )
+        self.grid = MultiGrid(
+            width=size,
+            height=size,
+            torus=True,
+            property_layers=[food_layer, pheromone_layer],
+        )
+        self.anthill_pos = (size // 2, size // 2)
         for i in range(n_ants):
             agent = Ant(self)
-            x,  = self.rng.integers(0, self.grid.width, size=1)
-            y,  = self.rng.integers(0, self.grid.height, size=1)
-            self.grid.place_agent(agent, (int(x),int(y)))
-         
-    def init_food(self, size, n_foodpile = 1):
-        food_layer = PropertyLayer(name = "food", width = size, height = size, default_value = 0, dtype = int)
+            (x,) = self.rng.integers(0, self.grid.width, size=1)
+            (y,) = self.rng.integers(0, self.grid.height, size=1)
+            self.grid.place_agent(agent, (int(x), int(y)))
+
+    def init_food(self, size, n_foodpile=1):
+        food_layer = PropertyLayer(
+            name="food", width=size, height=size, default_value=0, dtype=int
+        )
         n_foodpile = 1
         for _ in range(n_foodpile):
-            x, = self.rng.integers(0, size, size=1)
-            y, = self.rng.integers(0, size, size=1)
-            food_layer.set_cell((int(x),int(y)),40)
+            (x,) = self.rng.integers(0, size, size=1)
+            (y,) = self.rng.integers(0, size, size=1)
+            food_layer.set_cell((int(x), int(y)), 40)
         return food_layer
-            
+
     def step(self):
         for agent_class in self.agent_types:
             self.agents_by_type[agent_class].shuffle_do("step")
 
+
 def dist(point1, point2):
-    #manhattan distance
     return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
-    
+
+
 class Ant(mesa.Agent):
-    def __init__(self, model,carrying_capacity =1):
+    def __init__(self, model, carrying_capacity=1):
         super().__init__(model)
-        #TODO @property
+        # TODO @property
         self.food_bag = 0
         self.carrying_capacity = carrying_capacity
-        #self.model = model
+        # self.model = model
 
     def step(self):
         if self.food_bag == 0:
             self.explore()
         elif self.pos == self.model.anthill_pos:
-            self.food_bag -=1
+            self.food_bag -= 1
             print(f"Ant {self.unique_id} returned food!")
         else:
             self.return_to_anthill()
@@ -68,9 +76,12 @@ class Ant(mesa.Agent):
         #     move = move_candidates[0]
         # self.model.grid.move_agent(self, move)
         pass
+
     def explore(self):
-        #move to a random neighboring cell
+        # move to a random neighboring cell
         print("position is", type(self.pos), self.pos)
-        possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos, moore=True, include_center=False
+        )
         new_position = self.rng.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
--- src/ant_colony_optimization/viz.py
+++ src/ant_colony_optimization/viz.py
@@ -1,20 +1,23 @@
-from aco import Model, Ant
 from mesa.visualization import SolaraViz, make_space_component
 
+from ant_colony_optimization.core import Model
+
 model_params = {
-"size": 5,
-"n_ants": 10,
-"ant_vision": 1,
-"n_foodpile": 1,
+    "size": 5,
+    "n_ants": 10,
+    "ant_vision": 1,
+    "n_foodpile": 1,
 }
 
 model = Model(**model_params)
+
 
 def agent_portrayal(agent):
     return {
         "color": "tab:blue",
         "size": 50,
     }
+
 
 SpaceGraph = make_space_component(agent_portrayal)
 
@@ -24,4 +27,5 @@ page = SolaraViz(
     model_params=model_params,
     name="ACO",
 )
-#page
+# page
+

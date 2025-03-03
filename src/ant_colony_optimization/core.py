9f3d0733887d933a3571e5b3dbafa06fe6426771
Aaron Niskin <amniskin@gmail.com>
2025-03-02 22:17:36 -0800

    v0 is working but not converging yet...

--- src/ant_colony_optimization/core.py
+++ src/ant_colony_optimization/core.py
@@ -1,95 +1,138 @@
 import mesa
+import numpy as np
 from mesa.space import MultiGrid, PropertyLayer
-
-
-class Model(mesa.Model):
-    def __init__(self, size: int, n_ants: int, ant_vision=1, n_foodpile=1):
-        super().__init__()
-        self.n_ants = n_ants
-        self.anthill_pos = (size // 2, size // 2)
-        food_layer = self.init_food(size, n_foodpile)
-        pheromone_layer = PropertyLayer(
-            name="pheromone", width=size, height=size, default_value=0, dtype=int
-        )
-        ant_colony = PropertyLayer(name="ant_colony", 
-                                   width=size, 
-                                   height=size, 
-                                   default_value=False, 
-                                   dtype=bool)
-        
-        ant_colony.set_cell(self.anthill_pos, True)
-        self.grid = MultiGrid(
-            width=size,
-            height=size,
-            torus=True,
-           property_layers=[food_layer, pheromone_layer, ant_colony],
-        )
-
-        for i in range(n_ants):
-            agent = Ant(self)
-            (x,) = self.rng.integers(0, self.grid.width, size=1)
-            (y,) = self.rng.integers(0, self.grid.height, size=1)
-            self.grid.place_agent(agent, (int(x), int(y)))
-
-    def init_food(self, size, n_foodpile=1):
-        food_layer = PropertyLayer(
-            name="food", width=size, height=size, default_value=0, dtype=int
-        )
-        n_foodpile = 1
-        for _ in range(n_foodpile):
-            food_layer.set_cell((0,0), 40)
-        return food_layer
-
-    def step(self):
-        for agent_class in self.agent_types:
-            self.agents_by_type[agent_class].shuffle_do("step")
 
 
 def dist(point1, point2):
     return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
 
 
-class Ant(mesa.Agent):
-    def __init__(self, model, carrying_capacity=1):
-        super().__init__(model)
-        # TODO @property
-        self.food_bag = 0
-        self.carrying_capacity = carrying_capacity
-        # self.model = model
+class PheromoneLayer(PropertyLayer):
+    propertylayer_experimental_warning_given = True
+
+    def __init__(self, size, max_val=20, init_val=2, decay_factor=0.9, exp=2):
+        super().__init__(
+            name="pheromone",
+            width=size,
+            height=size,
+            default_value=init_val,
+            dtype=np.int32,
+        )
+        self.exp = exp
+        self.max_val = max_val
+        self.init_val = init_val
+        self.decay_factor = decay_factor
+
+    def amount(self, positions):
+        return np.array([self.data[x] for x in positions])
+
+    def decay(self):
+        self.modify_cells(lambda x: x * self.decay_factor)
+
+    def drop(self, pos, val):
+        val = 1 + val
+        self.modify_cell(pos, lambda x: min(x + val**self.exp, self.max_val))
+
+
+class FoodLayer(PropertyLayer):
+    propertylayer_experimental_warning_given = True
+
+    def __init__(self, size, random, n_food=40, num_sources=2):
+        super().__init__(
+            name="food", width=size, height=size, default_value=False, dtype=np.int32
+        )
+        self.n_food = n_food
+        self.num_food_sources = num_sources
+        for _ in range(num_sources):
+            cells = self.select_cells(lambda x: x == 0)
+            pos = random.choice(cells)
+            self.set_cell(pos, n_food)
+
+    def take(self, pos):
+        val = self.data[pos]
+        if val > 0:
+            self.set_cell(pos, val - 1)
+            return True
+        return False
+
+
+class Model(mesa.Model):
+    def __init__(
+        self,
+        size=11,
+        num_ants=10,
+        temperature=1,
+        num_food=40,
+        num_food_sources=2,
+        food_decay=0.99,
+        pheromone_exp=1.2,
+        pheromone_max=20,
+        pheromone_init=4,
+        pheromone_decay=0.99,
+    ):
+        super().__init__()
+        self.size = size
+        self.home = (size // 2, size // 2)
+        self.food = FoodLayer(
+            size, self.random, n_food=num_food, num_sources=num_food_sources
+        )
+        self.pheromone = PheromoneLayer(
+            size=size,
+            exp=pheromone_exp,
+            max_val=pheromone_max,
+            init_val=pheromone_init,
+            decay_factor=pheromone_decay,
+        )
+        ant_colony = PropertyLayer(
+            name="ant_colony", width=size, height=size, default_value=False, dtype=bool
+        )
+        ant_colony.set_cell(self.home, True)
+        self.grid = MultiGrid(
+            width=size,
+            height=size,
+            torus=False,
+            property_layers=[self.food, self.pheromone, ant_colony],
+        )
+        for i in range(num_ants):
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(
+                Ant(self, temperature=temperature, food_decay=food_decay), (x, y)
+            )
 
     def step(self):
-        if self.food_bag == 0:
-            self.explore()
-        elif self.pos == self.model.anthill_pos:
-            self.food_bag -= 1
-            print(f"Ant {self.unique_id} returned food!")
-        else:
-            self.return_to_anthill()
+        self.pheromone.decay()
+        for agent_class in self.agent_types:
+            self.agents_by_type[agent_class].shuffle_do("step")
 
-    def return_to_anthill(self):
-        # #we assume ants remember the way back to the anthill
-        # #compute the closet cell to the anthill
-        # #which neighboring cell is closest to anthill?
-        # neighbor_distances = {}
-        # for neighbor_cell in self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False):
-        #     neighbor_distances[neighbor_cell] = dist(neighbor_cell, self.anthill_pos)
-        # #if there is a tie, choose a random one
-        # move_candidates = [k for k, v in neighbor_distances.items() if v == min(neighbor_distances.values())]
-        # if len(move_candidates) > 1:
-        #     move = self.rng.choice(move_candidates)
-        # else:
-        #     move = move_candidates[0]
-        # self.model.grid.move_agent(self, move)
-        pass
 
-    def explore(self):
-        # move to a random neighboring cell
-        print("position is", type(self.pos), self.pos)
-        if not isinstance(self.pos, tuple):
-            print("not a tuple")
-        possible_steps = self.model.grid.get_neighborhood(
+class Ant(mesa.Agent):
+    def __init__(self, model, temperature=1, food_decay=0.9):
+        super().__init__(model)
+        self.pheromone = self.model.pheromone
+        self.temperature = temperature
+        self.decay = food_decay
+        self.food = 0
+
+    @property
+    def neighbors(self):
+        return self.model.grid.get_neighborhood(
             self.pos, moore=True, include_center=False
         )
-        print("after posn",  type(self.pos),self.pos,)
-        new_position = tuple(self.rng.choice(possible_steps))
-        self.model.grid.move_agent(self, new_position)
+
+    def step(self):
+        if self.pos == self.model.home:
+            self.food = 0
+        elif self.food == 0 and self.model.food.take(self.pos):
+            self.food += 1
+        self.pheromone.drop(self.pos, self.food)
+        if self.food > 0:
+            self.food *= self.decay
+            pos = sorted(self.neighbors, key=lambda x: dist(x, self.model.home))[0]
+        else:
+            wts = self.pheromone.amount(self.neighbors)
+            if self.temperature > 0:
+                wts = np.exp(wts / self.temperature)
+            wts = wts / wts.sum()
+            (pos,) = self.random.choices(self.neighbors, wts)
+        self.model.grid.move_agent(self, tuple(pos))
--- src/ant_colony_optimization/viz.py
+++ src/ant_colony_optimization/viz.py
@@ -3,13 +3,68 @@ from mesa.visualization import SolaraViz, make_space_component
 from ant_colony_optimization.core import Model
 
 model_params = {
-    "size": 5,
-    "n_ants": 10,
-    "ant_vision": 1,
-    "n_foodpile": 1,
+    "size": {
+        "type": "SliderInt",
+        "label": "size:",
+        "value": 30,
+        "min": 5,
+        "max": 30,
+        "step": 1,
+    },
+    "num_ants": {
+        "type": "SliderInt",
+        "label": "Number of ants:",
+        "value": 15,
+        "min": 1,
+        "max": 20,
+        "step": 1,
+    },
+    "temperature": {
+        "type": "SliderFloat",
+        "label": "temperature:",
+        "value": 2,
+        "min": 0,
+        "max": 5,
+        "step": 0.2,
+    },
+    "pheromone_exp": {
+        "type": "SliderFloat",
+        "label": "success exp:",
+        "value": 2.1,
+        "min": 1,
+        "max": 10,
+        "step": 0.1,
+    },
+    "num_food": 50,
+    "num_food_sources": {
+        "type": "SliderInt",
+        "label": "number of food sources:",
+        "value": 2,
+        "min": 1,
+        "max": 10,
+        "step": 1,
+    },
+    "food_decay": {
+        "type": "SliderFloat",
+        "label": "food decay factor:",
+        "value": 0.99,
+        "min": 0.75,
+        "max": 1.0,
+        "step": 0.01,
+    },
+    "pheromone_max": 100,
+    "pheromone_init": 10,
+    "pheromone_decay": {
+        "type": "SliderFloat",
+        "label": "decay factor:",
+        "value": 0.8,
+        "min": 0.1,
+        "max": 1.0,
+        "step": 0.05,
+    },
 }
 
-model = Model(**model_params)
+model = Model()
 
 
 def agent_portrayal(agent):
@@ -21,18 +76,30 @@ def agent_portrayal(agent):
 
 property_portrayal = {
     "food": {
-    "color": "tab:red",
-   # "size": 10,
-    "alpha": 0.8
+        "color": "tab:red",
+        # "size": 10,
+        "alpha": 0.8,
     },
     "ant_colony": {
         "color": "tab:green",
-     #   "size": 10,
-        "alpha": 0.8
-    }
+        "stroke": "blue",
+        #   "size": 10,
+        "alpha": 0.8,
+        "colorbar": False,
+    },
+    "pheromone": {
+        "color": "blue",
+        "stroke": "blue",
+        # "size": 10,
+        "alpha": 0.8,
+        "vmin": 0,
+        "vmax": model_params["pheromone_max"],
+    },
 }
 
-SpaceGraph = make_space_component(agent_portrayal, propertylayer_portrayal=property_portrayal)
+SpaceGraph = make_space_component(
+    agent_portrayal, propertylayer_portrayal=property_portrayal
+)
 
 page = SolaraViz(
     model,
@@ -40,5 +107,3 @@ page = SolaraViz(
     model_params=model_params,
     name="ACO",
 )
-page
-
